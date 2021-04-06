8623b9c151c7c399da23f2c1346f1b7a0d3e6480
siva4488 <siva4488@users.noreply.github.com>
2021-04-06 11:26:39 +0530

    Update output.tf
    debugging Public Ip Address

--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "resource_group_name" {
 output "network_security_group_name" {
   value = "${azurerm_network_security_group.default.name}"
 }
+output "azurevm_public_ip_address" {
+  value = "${azurerm_public_ip.instances[0].ip_address}"
+}
