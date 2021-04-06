87323959fe55ce1ab60f5ddfcb16061379e02ca6
siva4488 <siva4488@users.noreply.github.com>
2021-04-06 12:08:29 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,6 +4,3 @@ output "resource_group_name" {
 output "network_security_group_name" {
   value = "${azurerm_network_security_group.default.name}"
 }
-output "public_ip_address" {
-  value = data.azurerm_public_ip.instances[0].ip_address
-}
