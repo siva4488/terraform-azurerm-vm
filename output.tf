e0b8fa072548f09e47a79181972004057fd62a58
siva4488 <siva4488@users.noreply.github.com>
2021-04-06 11:00:58 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,27 +4,3 @@ output "resource_group_name" {
 output "network_security_group_name" {
   value = "${azurerm_network_security_group.default.name}"
 }
-output "object" {
-  description = "Output the full object"
-  value = azurerm_public_ip.public_ip
-}
-
-output "ip_address" {
-  description = "Output the ip address"
-  value = azurerm_public_ip.public_ip.ip_address
-}
-
-output "fqdn" {
-  description = "Output the fully qualified domain name"
-  value = azurerm_public_ip.public_ip.fqdn
-}
-
-output "name" {
-  description = "Output the object name"
-  value = azurerm_public_ip.public_ip.name
-}
-
-output "id" {
-  description = "Output the object ID"
-  value = azurerm_public_ip.public_ip.id
-}
